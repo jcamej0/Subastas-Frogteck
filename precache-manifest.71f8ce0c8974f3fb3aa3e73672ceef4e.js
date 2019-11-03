66702bc846056910bb896e1ec1a57ae0ef5af9f2
Juan Camejo <jcamejob@gmail.com>
2019-11-03 01:22:24 +0100

    Updates

---
+++ precache-manifest.71f8ce0c8974f3fb3aa3e73672ceef4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcce088a032b41f14d2e71bfb82352e",
+    "url": "/subastas-frogteck/index.html"
+  },
+  {
+    "revision": "61a2c5296fe0748e8691",
+    "url": "/subastas-frogteck/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "99aa61baa4899ea8dafd",
+    "url": "/subastas-frogteck/static/css/main.13fdde88.chunk.css"
+  },
+  {
+    "revision": "61a2c5296fe0748e8691",
+    "url": "/subastas-frogteck/static/js/2.e1bdc73a.chunk.js"
+  },
+  {
+    "revision": "99aa61baa4899ea8dafd",
+    "url": "/subastas-frogteck/static/js/main.850acfe7.chunk.js"
+  },
+  {
+    "revision": "b290e609475d4dc93ef8",
+    "url": "/subastas-frogteck/static/js/runtime-main.3c993b87.js"
+  },
+  {
+    "revision": "956d630306130d9e065211adc8a11a4c",
+    "url": "/subastas-frogteck/static/media/cart.956d6303.svg"
+  },
+  {
+    "revision": "311313cded0dc23bba479b786ac0aad8",
+    "url": "/subastas-frogteck/static/media/plus.311313cd.svg"
+  },
+  {
+    "revision": "dbf3ce0d9b43fd7187f02d893edcb72e",
+    "url": "/subastas-frogteck/static/media/tag.dbf3ce0d.svg"
+  }
+]);
